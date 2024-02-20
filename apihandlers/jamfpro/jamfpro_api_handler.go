12dbde0ddbf4896219ca02c77c3e5322213f5f69
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-20 12:00:16 +0000

    Refactor EndpointConfig struct and remove duplicate definition

--- apihandlers/jamfpro/jamfpro_api_handler.go
+++ apihandlers/jamfpro/jamfpro_api_handler.go
@@ -2,13 +2,6 @@ package jamfpro
 
 import "github.com/deploymenttheory/go-api-http-client/logger"
 
-// EndpointConfig is a struct that holds configuration details for a specific API endpoint.
-// It includes what type of content it can accept and what content type it should send.
-type EndpointConfig struct {
-	Accept      string  `json:"accept"`       // Accept specifies the MIME type the endpoint can handle in responses.
-	ContentType *string `json:"content_type"` // ContentType, if not nil, specifies the MIME type to set for requests sent to the endpoint. A pointer is used to distinguish between a missing field and an empty string.
-}
-
 // JamfAPIHandler implements the APIHandler interface for the Jamf Pro API.
 type JamfAPIHandler struct {
 	OverrideBaseDomain string        // OverrideBaseDomain is used to override the base domain for URL construction.
