c87811a81dbf713e1598b7fe6081d12f8e710300
ShocOne <62835948+ShocOne@users.noreply.github.com>
2024-02-20 05:47:14 +0000

    Add support for additional HTTP methods***

--- httpclient/httpclient_methods.go
+++ httpclient/httpclient_methods.go
@@ -1,4 +1,6 @@
 // httpclient_methods.go
+package httpclient
+
 /* Ref: https://www.rfc-editor.org/rfc/rfc7231#section-8.1.3
 
 +---------+------+------------+
@@ -12,18 +14,20 @@
 | POST    | no   | no         |
 | PUT     | no   | yes        |
 | TRACE   | yes  | yes        |
-+---------+------+------------+  
++---------+------+------------+
 */
-package httpclient
 
 import "net/http"
 
 // IsIdempotentHTTPMethod checks if the given HTTP method is idempotent.
 func IsIdempotentHTTPMethod(method string) bool {
 	idempotentHTTPMethods := map[string]bool{
-		http.MethodGet:    true,
-		http.MethodPut:    true,
-		http.MethodDelete: true,
+		http.MethodGet:     true,
+		http.MethodPut:     true,
+		http.MethodDelete:  true,
+		http.MethodHead:    true,
+		http.MethodOptions: true,
+		http.MethodTrace:   true,
 	}
 
 	return idempotentHTTPMethods[method]
@@ -33,8 +37,9 @@ func IsIdempotentHTTPMethod(method string) bool {
 // PATCH can be idempotent but often isn't used as such.
 func IsNonIdempotentHTTPMethod(method string) bool {
 	nonIdempotentHTTPMethods := map[string]bool{
-		http.MethodPost:  true,
-		http.MethodPatch: true,
+		http.MethodPost:    true,
+		http.MethodPatch:   true,
+		http.MethodConnect: true,
 	}
 
 	return nonIdempotentHTTPMethods[method]
