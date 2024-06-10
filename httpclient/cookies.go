96652c9103c10cde5af62b8911f693fcf58388ec
Joseph Little <joelittle11@live.com>
2024-06-10 15:50:20 +0100

    debug

--- httpclient/cookies.go
+++ httpclient/cookies.go
@@ -12,19 +12,16 @@ func (c *Client) parseCustomCookies(cookiesList []*http.Cookie) error {
 	if err != nil {
 		return err
 	}
-	c.Logger.Debug("FLAG-1")
 	cookieUrl, err := url.Parse((*c.Integration).Domain())
-	c.Logger.Debug(cookieUrl.Host)
-	c.Logger.Debug("FLAG-2")
+
 	if err != nil {
 		return err
 	}
-	c.Logger.Debug("FLAG-3")
-	c.Logger.Debug(fmt.Sprintf("%+v", cookiesList))
-	c.Logger.Debug(fmt.Sprintf("%+v", c.http.Jar))
+
 	c.http.Jar = cookieJar
 	c.http.Jar.SetCookies(cookieUrl, cookiesList)
-	c.Logger.Debug("FLAG-4")
+
+	c.Logger.Debug(fmt.Sprintf("%+v", c.http.Jar))
 
 	return nil
 }
