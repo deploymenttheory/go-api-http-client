9028167fb8452f551c6f1184b86e3153482fce6a
Joseph Little <joelittle11@live.com>
2024-07-24 15:21:56 +0100

    testing implementations

--- httpclient/http.go
+++ httpclient/http.go
@@ -1,10 +1,14 @@
 package httpclient
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"time"
 )
+
+// Production
 
 type ProdClient struct {
 	*http.Client
@@ -30,10 +34,52 @@ func (c *ProdClient) SetRedirectPolicy(policy *func(req *http.Request, via []*ht
 	c.CheckRedirect = *policy
 }
 
-type testClient struct {
+// Mocking
+
+type mockClient struct {
+	lockedResponseCode int
 }
 
-func (m *testClient) Do(req *http.Request) (*http.Response, error) {
-	// do some stuff which makes a response you like
-	return nil, nil
+func (m *mockClient) CloseIdleConnections() {
+	panic("invalid function call")
 }
+
+func (m *mockClient) Do(req *http.Request) (*http.Response, error) {
+	statusString := http.StatusText(m.lockedResponseCode)
+
+	if statusString == "" {
+		return nil, fmt.Errorf("unknown response code requested: %d", m.lockedResponseCode)
+	}
+
+	response := &http.Response{StatusCode: m.lockedResponseCode}
+
+	return response, nil
+}
+
+func (m *mockClient) Get(_ string) (*http.Response, error) {
+	return m.Do(nil)
+}
+
+func (m *mockClient) Head(_ string) (*http.Response, error) {
+	return m.Do(nil)
+}
+
+func (m *mockClient) Post(_ string, _ string, _ io.Reader) (*http.Response, error) {
+	return m.Do(nil)
+}
+
+func (m *mockClient) PostForm(_ string, _ url.Values) (*http.Response, error) {
+	return m.Do(nil)
+}
+
+func (m *mockClient) SetCookieJar(jar http.CookieJar) {}
+
+func (m *mockClient) SetCookies(url *url.URL, cookies []*http.Cookie) {}
+
+func (m *mockClient) SetCustomTimeout(time.Duration) {}
+
+func (m *mockClient) Cookies(*url.URL) []*http.Cookie {
+	return nil
+}
+
+func (m *mockClient) SetRedirectPolicy(*func(req *http.Request, via []*http.Request) error) {}
