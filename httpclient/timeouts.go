c6db71bd7cb4225512665c14d7638aafb6db457f
Joseph <joelittle11@live.com>
2025-03-13 14:52:56 +0000

    removed mutex

--- httpclient/timeouts.go
+++ httpclient/timeouts.go
@@ -1,22 +1,15 @@
 package httpclient
 
 import (
-	"sync"
 	"time"
 )
 
-var mu sync.Mutex
-
 // Amends the HTTP timeout time
 func (c *Client) ModifyHttpTimeout(newTimeout time.Duration) {
-	mu.Lock()
-	defer mu.Unlock()
 	c.http.Timeout = newTimeout
 }
 
 // Resets HTTP timeout time back to 10 seconds
 func (c *Client) ResetTimeout() {
-	mu.Lock()
-	defer mu.Unlock()
 	c.http.Timeout = DefaultTimeout
 }
